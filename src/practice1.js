75022a91b798d2393e809e4b2dcb61df7334373c
XiongqiChang <2838159948@qq.com>
2020-10-16 22:18:06 +0800

    完成代码

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,25 @@
-const parseData
+const parseData = function (inputdata) {
+    
+
+    let data = inputdata.data;
+    let column = inputdata.column;
+    let result = [];
+
+  for (let dataItem of data) {
+        let newObj = {};
+        let  index = 0;
+
+        for (let nameItem of column) {
+            newObj[nameItem.name] = dataItem[index];
+            index++;
+        }
+        result.push(newObj);
+    }
+
+    return result;
+    
+
+
+   
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+
+
+    let result = [...items];
+    sections.forEach((element,i) => {
+        result.splice(element.index + i, 0, element.content);
+    });
+
+     return result;
+  };
+  export { inject };
+  
